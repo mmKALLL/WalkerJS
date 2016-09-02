8a8a63986d250f99f2c766953ca4229c9077f24a
Esa Koskinen <esa.koskinen@relexsolutions.com>
2016-09-02 23:21:40 +0900

    Made the code much cleaner; added objects for global things.

--- script/walker.js
+++ script/walker.js
@@ -6,22 +6,30 @@
     forwardSteps: 0,
     backwardSteps: 0,
     totalSteps: function() { return this.forwardSteps + this.backwardSteps; },
+    position: function() { return this.forwardSteps - this.backwardSteps; },
+
+    stepForward: function() {
+      this.forwardSteps += 1;
+      this.totalSteps() <= 1 ?
+      messagebox.pushMessage("You clicked the button!") :
+      messagebox.pushMessage("You clicked the button " + this.totalSteps() + " times!");
+    },
+
+    stepBackward: function() {
+      this.backwardSteps += 1;
+      this.backwardSteps <= 1 ?
+      messagebox.pushMessage("You took a step back!") :
+      messagebox.pushMessage("You have taken a step back " + this.backwardSteps + " times!");
+    },
+
   };
 
-
-  document.getElementById("testbutton").addEventListener("click", function(){
-    player.forwardSteps += 1;
-    player.totalSteps() < 2 ?
-    document.getElementById("div").innerHTML += "<br>You clicked the button!" :
-    document.getElementById("div").innerHTML += "<br>You clicked the button " + player.totalSteps() +
-                                                " times!";
-  });
-
-  function playerTakeStep() {
-
-
+  var messagebox = {
+    elem: document.getElementById("messagebox"),
+    pushMessage: function(string) { this.elem.innerHTML = string + "<br>" + this.elem.innerHTML; },
   }
 
-
+  document.getElementById("stepForwardButton").addEventListener("click", player.stepForward);
+  document.getElementById("stepBackwardButton").addEventListener("click", player.stepBackward);
 
 })();
