30c3bf59556a70448a88f064456f01fdcc1da7e5
mmKALLL <esakoskin2@gmail.com>
2018-02-12 21:59:32 +0900

    Added button for returning to title screen.

--- script/walker.js
+++ script/walker.js
@@ -141,6 +141,10 @@
     document.getElementById("decreaseMoodButtonG").addEventListener("click", player.changeMoodFunc(-30, "g"));
     document.getElementById("decreaseMoodButtonB").addEventListener("click", player.changeMoodFunc(-30, "b"));
     
+    document.getElementById("titleReturnButton").addEventListener("click", function() {
+      window.location.replace("./index.html");
+    });
+    
     var elem = document.documentElement;
     elem.style.setProperty("--mood-background-color", "white");
     elem.style.setProperty("--mood-text-color", "black");
