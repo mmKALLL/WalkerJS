9512528b723e65e5dac09d78b87541c68734203c
mmKALLL <esakoskin2@gmail.com>
2018-04-14 22:29:57 +0900

    Updated roadmap, added new ideas and improved priorization.

--- script/walker.js
+++ script/walker.js
@@ -25,19 +25,23 @@
   // TODO: Add text bunching ("Took step! (x19)" -> "Took step! (x20)" when same message is repeated.)
   
   // TODO: Saving and loading from localstorage. Handle in separate thread which calls message function when done.
+  // TODO: Load game button on title screen when saved data exists (use spinner if checking localstorage takes long).
   // TODO: Implement the upgrade system (step back/forward, look back, look forward).
   // TODO: Implement the energy system (running and walking cost energy, food/breaks/sleep restore it).
   // TODO: Keep track of player inventory.
   // TODO: Keep track of position, place items on map.
+  // TODO: Implement the trading system.
   // TODO: Implement houses, towns, cities, and the Capital, as well as merchants and other random encounters.
   
   // TODO: Cutscenes. Fade out .gameplay, fade in/out text that floats up or down.
   // TODO: Control mood within cutscenes.
+  // TODO: Day system and victory/loss conditions. At start of day, inform that game is saved, fade back after button click.
   // TODO: Add some interesting messages and world events.
   // TODO: Implement The Friend.
   // TODO: Implement a pause button, and pause game during cutscenes.
   // TODO: Implement endings.
   
+  // DONE: Use custom domain with GitHub Pages. ( walkergame.com )
   // EXTEND: Export/import saves as strings.
   // EXTEND: Full screen button.
   // EXTEND: Fade out buttons on cutscene/return to title.
